c645ffe279f6b24dba5bf0d71faf0262c786a6ce
mepcovenkat97 <mepcovenkat97@gmail.com>
2019-11-15 10:36:31 +0530

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const { addUser, removeUser, getUser, getUserInRoom } = require('./users')
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router')
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+
+   socket.on('join',({ name, room}, callback) => {
+      const { error, user } = addUser({ id: socket.id, name, room});
+      if(error) return callback(error);
+
+      // Welcoming in personal
+      socket.emit('message', { user:"admin", text:"Welcome to the world of Conversation"});
+      //Welcoming inside the group (Public Welcome)
+      socket.broadcast.to(user.room).emit('message',{user:"admin", text:`${user.name} has joined the group`})
+
+      socket.join(user.room);
+
+      callback();
+   })
+//
+   socket.on('sendMessage',(message, callback) => {
+      const user = getUser(socket.id);
+      io.to(user.room).emit('message',{ user:user.name, text:message});
+      callback()
+   })
+
+   socket.on('disconnect', () => {
+      console.log("User had Left");
+   })
+})
+
+app.use(router);
+
+server.listen(PORT, () => {
+   console.log(`Server started on port ${PORT}`)
+})
---
+++ frontend/src/components/Chat/chat.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+import './chat.css';
+
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+let socket;
+
+const Chat = ({ location }) => {
+
+   //similar to state variable in class based components
+   const [name, setName] = useState('');
+   const [room, setRoom] = useState('');
+   const [messages, setMessages] = useState([]);
+   const [message, setMessage] = useState('');
+   const ENDPOINT = 'localhost:5000';
+
+   //useEffect - similar to componentDidMount but will render all the times (similar to combining all the ComponentDid...() functions)
+   useEffect(()=>{
+      const { name, room } = queryString.parse(location.search);
+
+      socket = io(ENDPOINT);
+
+      setName(name);
+      setRoom(room);
+
+      socket.emit('join',{ name, room}, () => {
+        
+      })
+
+      return () => {
+         socket.emit('disconnect');
+         socket.off();
+      }
+
+   },[ENDPOINT, location.search])
+   //[ENDPOINT, location.search] - indicates useEffect will render only if there is a change in these values
+//
+   useEffect(() => {
+      socket.on('message', (message)=> {
+         setMessages([...messages,message])
+      })
+   },[messages])
+
+   const sendMessage = (event) =>{
+      event.preventDefault();
+      if(message)
+      {
+         socket.emit('sendMessage', message, () => setMessage(''))
+      }
+   } 
+
+   console.log(message, messages);
+
+   return (
+      <div className="outerContainer">
+         <div className="container">
+            <InfoBar room={room}/>
+            <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+         </div>
+      </div>
+   )
+}
+
+export default Chat;
